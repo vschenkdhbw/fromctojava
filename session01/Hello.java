ae898c80f204ee74788eb4f0b64b20774ac3328b
vschenkdhbw <70517588+vschenkdhbw@users.noreply.github.com>
2026-05-29 13:23:11 +0000

    adding cool stuff

---
+++ session01/Hello.java
@@ -0,0 +1,10 @@
+public class Hello { 
+    public static void main(String[] args) { 
+        System.out.println("Hallo, Java!"); 
+        int zahl = 42; 
+        System.out.println("Die Zahl ist: " + zahl); 
+        double temp = 19.3; 
+        System.out.println("Temperatur: " + temp); 
+        System.err.println("Temperatur: " + temp);
+    } 
+} 
